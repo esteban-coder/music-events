4cf5457120e4f1521cc21528c565a1851c352302
Esteban Aliaga <estaliaga@gmail.com>
2024-04-22 19:21:11 -0500

    initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
 {
@@ -6,13 +10,12 @@ path: '',
 pathMatch: 'full',
 redirectTo: 'home',
 },
+{ path: 'home', pathMatch: 'full', component: HomeComponent },
+{ path: 'login', pathMatch: 'full', component: LoginComponent },
+{ path: 'register', pathMatch: 'full', component: RegisterComponent },
 {
-    
-}
-
-
-
-
-
-
+  path: 'forgot-password',
+  pathMatch: 'full',
+  component: ForgotPasswordComponent,
+},
 ];
